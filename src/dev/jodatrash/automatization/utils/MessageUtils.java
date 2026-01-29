6eb616b8ca935e4902fcbd3ae2d1a0e38ba6145b
Jonathan Pacheco Cruz <JonathanPCruz.dev@hotmail.com>
2026-01-28 23:56:53 -0600

    feat: Add a class to handle messages

---
+++ src/dev/jodatrash/automatization/utils/MessageUtils.java
@@ -0,0 +1,7 @@
+package dev.jodatrash.automatization.utils;
+
+public class MessageUtils {
+  public static final String INVALID_MESSAGE = "La opción ingresada no es valida, verifica y vuelve a intentarlo.";
+  public static final String WEB_CHECK = "Paso 1.- Testeando que la pagina responda.";
+  public static final String WEB_LOGIN = "Paso 2.- Testeando el inicio de sesión.";
+}
