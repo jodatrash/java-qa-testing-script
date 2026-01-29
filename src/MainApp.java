ee7caba87d46f0440ac202a580f8b27d6a61ab1e
Jonathan Pacheco Cruz <JonathanPCruz.dev@hotmail.com>
2026-01-28 23:44:29 -0600

    refactor: Refactoring the main class MainApp.java

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,32 +1,12 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import dev.jodatrash.automatization.menu.ApplicationWebDriver;
+import dev.jodatrash.automatization.menu.MenuSelection;
 
 public class MainApp {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Testing QA con selenium WebDriver");
-        System.out.println("Iniciando WebDriver...");
+  public static void main(String[] args) throws Exception {
 
-        WebDriver driver = new ChromeDriver();
+    MenuSelection menuSelection = new MenuSelection();
+    ApplicationWebDriver appDriver = new ApplicationWebDriver(menuSelection);
+    appDriver.startApplication();
 
-        driver.get("https://demowebshop.tricentis.com/");
-        String title = driver.getTitle();
-        System.out.println("Titulo es: " + title);
-        driver.manage().window().maximize();
-        driver.findElement(By.linkText("Log in")).click();
-        driver.findElement(By.xpath("//*[@id='Email']")).sendKeys(" ");
-        driver.findElement(By.xpath("//*[@id='Password']")).sendKeys(" ");
-        driver.findElement(By.xpath("/html/body/div[4]/div[1]/div[4]/div[2]/div/div[2]/div[1]/div[2]/div[2]/form/div[5]/input")).click();
-        driver.findElement(By.partialLinkText("Apparel & Shoes")).click();
-        driver.findElement(By.partialLinkText("Blue Jeans")).click();
-        driver.findElement(By.id("addtocart_36_EnteredQuantity")).click();
-        driver.findElement(By.id("addtocart_36_EnteredQuantity")).clear();
-        driver.findElement(By.id("addtocart_36_EnteredQuantity")).sendKeys("4");
-        driver.findElement(By.id("add-to-cart-button-36")).click();
-        driver.findElement(By.linkText("Shopping cart")).click();
-        Thread.sleep(30000);
-        driver.close();
-        System.out.println("fin del aplicativo.");
-
-    }
+  }
 }
