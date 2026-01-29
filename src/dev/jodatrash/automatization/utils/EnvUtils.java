0fbddfabd12b88c9ec078537fd86b3e14168e5f4
Jonathan Pacheco Cruz <JonathanPCruz.dev@hotmail.com>
2026-01-28 23:49:05 -0600

    feat: Add a class to handle environment variables

---
+++ src/dev/jodatrash/automatization/utils/EnvUtils.java
@@ -0,0 +1,6 @@
+package dev.jodatrash.automatization.utils;
+
+public class EnvUtils {
+  public static final String EMAIL = System.getenv("USER_EMAIL");
+  public static final String PASSWORD = System.getenv("PASSWORD");
+}
