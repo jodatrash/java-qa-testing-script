9d6411cbe548a5f376869477af12c1b72480111d
Jonathan Pacheco Cruz <JonathanPCruz.dev@hotmail.com>
2026-02-01 19:58:26 -0600

    feat: add NO_AVAILABLE message

--- src/dev/jodatrash/automatization/utils/MessageUtils.java
+++ src/dev/jodatrash/automatization/utils/MessageUtils.java
@@ -1,7 +1,8 @@
 package dev.jodatrash.automatization.utils;
 
 public class MessageUtils {
-  public static final String INVALID_MESSAGE = "La opción ingresada no es valida, verifica y vuelve a intentarlo.";
+  public static final String INVALID_OPTION = "La opción ingresada no es valida, verifica y vuelve a intentarlo.";
+  public static final String NO_AVAILABLE= "La opción no existe.";
   public static final String WEB_CHECK = "Paso 1.- Testeando que la pagina responda.";
   public static final String WEB_LOGIN = "Paso 2.- Testeando el inicio de sesión.";
 }
